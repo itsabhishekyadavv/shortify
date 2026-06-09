3f7f5d5082f5ede6247fd492ce806f69603bbc2b
itsabhishekyadavv <itsabhishekyadavv@gmail.com>
2026-06-09 13:38:26 +0530

    API Fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://shortify-backend-4wrf.onrender.com",
+    "https://shortify-backend-4wrf.onrender.com/api",
 });
 
 API.interceptors.request.use(
