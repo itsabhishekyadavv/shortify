fde6a973aa24819d9add11e505b7fd6da623b315
itsabhishekyadavv <itsabhishekyadavv@gmail.com>
2026-06-09 13:23:18 +0530

    Production API Updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://shortify-backend-4wrf.onrender.com",
 });
 
 API.interceptors.request.use(
